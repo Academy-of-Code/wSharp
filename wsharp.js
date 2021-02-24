98e66e6e3f6b26cbd2f1dea5e3335a2c279b4286
Academy-of-Code <77977795+Academy-of-Code@users.noreply.github.com>
2021-02-24 13:03:21 -0500

    Update wsharp.js

--- wsharp.js
+++ wsharp.js
@@ -1,17 +1,18 @@
 const if_char = 'Þ';
 const else_char = '₤';
 const elseif_char = '₤Þ';
-const while_char ="bnklewndbchnfkedcbh"
-const var_char="bnklewndbchnfkedcbh";
-const function_char="bnklewndbchnfkedcbh";
-const const_char="bnklewndbchnfkedcbh";
-const let_char="bnklewndbchnfkedcbh";
-const return_char="bnklewndbchnfkedcbh";
-const class_char="bnklewndbchnfkedcbh";
+const while_char = "bnklewndbchnfkedcbh"
+const var_char = "ὲ";
+const function_char = "bnklewndbchnfkedcbh";
+const const_char = "bnklewndbchnfkedcbh";
+const let_char = "bnklewndbchnfkedcbh";
+const return_char = "bnklewndbchnfkedcbh";
+const class_char = "bnklewndbchnfkedcbh";
 var input;
 var output;
-function replace(){
-	output = input.replace(if_char, "if")
+
+function replace() {
+  output = input.replace(if_char, "if")
   output = output.replace(else_char, "else")
   output = output.replace(elseif_char, "else if")
   output = output.replace(while_char, "while")
@@ -22,40 +23,43 @@ function replace(){
   output = output.replace(return_char, "return")
   output = output.replace(class_char, "class")
 }
-function compile(){
-	output = null
-  try{
-  	replace();
+
+function compile() {
+  output = null
+  try {
+    replace();
     eval(output);
-  } catch (err){
-  	output = err
+  } catch (err) {
+    output = err
   }
-  /*Log Function*/ (function () {
+  /*Log Function*/
+  (function() {
     if (!console) {
-        console = {}
+      console = {}
     }
     var old = console.log
     var logger = output
-    console.log = function (message) {
-        if (typeof message == 'object') {
-            logger.value += (JSON && JSON.stringify ? JSON.stringify(message) : String(message))
-        } else {
-            logger.value += message
-        }
+    console.log = function(message) {
+      if (typeof message == 'object') {
+        logger.value += (JSON && JSON.stringify ? JSON.stringify(message) : String(message))
+      } else {
+        logger.value += message
+      }
     }
   })();
-/*Error Function*/ (function () {
+  /*Error Function*/
+  (function() {
     if (!console) {
-        console = {}
+      console = {}
     }
     var old = console.error
     var logger = output
-    console.error = function (message) {
-        if (typeof message == 'object') {
-            logger.value += (JSON && JSON.stringify ? JSON.stringify(message) : String(message))
-        } else {
-            logger.value += message
-        }
+    console.error = function(message) {
+      if (typeof message == 'object') {
+        logger.value += (JSON && JSON.stringify ? JSON.stringify(message) : String(message))
+      } else {
+        logger.value += message
+      }
     }
   })();
 }
